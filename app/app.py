3cdd276cdf201ee7d7b431851921d0f202381706
matias5000 <matii.torress@duocuc.cl>
2026-07-02 23:12:23 -0400

    segundo commit

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, make_response
 from markupsafe import escape
 from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+
 @app.after_request
 def security_headers(response):
     response.headers["Content-Security-Policy"] = (
@@ -14,11 +16,13 @@ def security_headers(response):
     )
     response.headers["X-Content-Type-Options"] = "nosniff"
     return response
+
 @app.route("/")
 def index():
     user_input = request.args.get("input", "Aplicacion segura EV3 funcionando")
     safe_input = escape(user_input)
     response = make_response(f"<h1>{safe_input}</h1>")
     return response
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)  # nosec B104
